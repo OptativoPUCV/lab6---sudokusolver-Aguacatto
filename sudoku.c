091757be220d69bf292af0ee3fad7210bbe11170
Your Name <you@example.com>
2024-05-18 01:57:28 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -69,15 +69,14 @@ int is_valid(Node* n){
 List* get_adj_nodes(Node* n)
 {
    List *list = createList();
-   int i, j, k;
 
-   for (i = 0; i < 9; i++)
+   for (int i = 0; i < 9; i++)
    {
-      for (j = 0; j < 9; j++) 
+      for (int j = 0; j < 9; j++) 
       {
          if (n->sudo[i][j] == 0)
          {
-            for (k = 1; k <= 9; k++)
+            for (int k = 1; k <= 9; k++)
             {
                Node *new_node = copy(n);
                new_node->sudo[i][j] = k;
